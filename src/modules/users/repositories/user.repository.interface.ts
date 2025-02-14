d9586eb9fd2bcf92ceccfe36e2c186f11d2dc14a
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2025-02-14 17:23:08 -0300

    feat(user): Adicionando interface de IUserRepository

---
+++ src/modules/users/repositories/user.repository.interface.ts
@@ -0,0 +1,6 @@
+import { CreateUserDto } from '../dto/create-user.dto';
+import { User } from '../entities/user.entity';
+
+export interface IUserRepository {
+  create(dto: CreateUserDto): Promise<User>;
+}
