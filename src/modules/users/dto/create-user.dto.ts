72c85cab41a0325ae34918ff9aeb43c6b36a7705
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2025-02-14 18:31:09 -0300

    feat:(user): Adicionando Validação no CreateUserDto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
   password: string;
 }
