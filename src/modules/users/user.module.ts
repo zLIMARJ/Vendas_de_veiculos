1fa9d65de5d1a09853ad1d4b694fb37dab113e24
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2025-02-14 18:38:13 -0300

    feat(users): configurar UsersModule com injeção de dependências
    - Importa PrismaModule para garantir acesso ao banco de dados
    - Registra PrismaService, CreateUserService e PasswordHasherService como providers
    - Define o token USER_REPOSITORY e o vincula à implementação UserRepository
    - Adiciona CreateUserController para lidar com requisições de criação de usuário

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserService } from './services/create-user.service';
+import { CreateUserController } from './controllers/create-user.controller';
+import { PasswordHasherService } from 'src/providers/password-hasher.provider';
+import { UserRepository } from './repositories/user.repository';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { USER_REPOSITORY } from './repositories/user.repository.interface';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [PrismaService],
+  imports: [PrismaModule],
+  controllers: [CreateUserController],
+  providers: [
+    PrismaService,
+    CreateUserService,
+    PasswordHasherService,
+    {
+      provide: USER_REPOSITORY,
+      useClass: UserRepository,
+    },
+  ],
 })
 export class UsersModule {}
